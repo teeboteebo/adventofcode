b385d499e83f4c8288572be11ee4e7d0bf9049ae
Jesper Asplund <jesper.asplund95@gmail.com>
2022-12-05 14:23:04 +0100

    day 4

---
+++ 2022/4/1.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const values = fs
+  .readFileSync("./input.txt", "utf-8")
+  .trim()
+  .split("\n")
+  .map((string) => string.split(","));
+
+const solution = () => {
+  return values.filter((pair) => {
+    const [elf1, elf2] = pair.map((elf) => elf.split("-").map(Number));
+
+    const [elf1Start, elf1End] = elf1;
+    const [elf2Start, elf2End] = elf2;
+
+    return (
+      (elf1Start <= elf2Start && elf1End >= elf2End) ||
+      (elf2Start <= elf1Start && elf2End >= elf1End)
+    );
+  }).length;
+};
+
+console.log(solution());
