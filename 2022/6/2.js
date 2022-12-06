876b9b8bbcd4fe8f9a868d72fab76046066e5f8b
Jesper Asplund <jesper.asplund95@gmail.com>
2022-12-06 09:55:13 +0100

    day 6

---
+++ 2022/6/2.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const datastream = fs.readFileSync("./input.txt", "utf-8").split("");
+
+const solution = () => {
+  const amountLookingFor = 14;
+  let segmentfound = false;
+  let numbersProcessed = amountLookingFor;
+  let i = 0;
+  while (!segmentfound && numbersProcessed < datastream.length) {
+    const segmentToCheck = datastream.slice(i, i + amountLookingFor);
+    const uniqueChars = new Set(segmentToCheck);
+    if (uniqueChars.size === amountLookingFor) {
+      segmentfound = true;
+    } else {
+      i++;
+      numbersProcessed++;
+    }
+  }
+  return numbersProcessed;
+};
+
+console.log(solution());
