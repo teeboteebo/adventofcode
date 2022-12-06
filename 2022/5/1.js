1cd6fdd2bf103ea56fdf4e76ff281a0d00d9d15b
Jesper Asplund <jesper.asplund95@gmail.com>
2022-12-06 09:23:54 +0100

    day 5

---
+++ 2022/5/1.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const values = fs
+  .readFileSync("./input.txt", "utf-8")
+  .split("\n\n")
+  .map((string) => string.split("\n"));
+
+const getStartingState = () => {
+  const startingPoint = {};
+  const rows = values[0].pop();
+  const itemRows = values[0];
+  rows.split("").forEach((char, i) => {
+    if (char === " ") return;
+
+    startingPoint[i] = {
+      items: [],
+      char,
+    };
+  });
+  itemRows.reverse().forEach((row) => {
+    const test = row.split("");
+    const validIndexes = Object.keys(startingPoint);
+    validIndexes.forEach(
+      (index) => test[index] && startingPoint[index].items.push(test[index])
+    );
+  });
+  const pretty = {};
+  Object.keys(startingPoint).forEach((key) => {
+    pretty[startingPoint[key].char] = startingPoint[key].items.filter(
+      (x) => x !== " "
+    );
+  });
+
+  return pretty;
+};
+
+const solution = () => {
+  const state = getStartingState();
+  const steps = values[1].map((step) => step.split(" "));
+  steps.forEach((step) => {
+    const [, amt, , from, , to] = step;
+    for(let i = 0; i < amt; i++) {
+      const item = state[from].pop();
+      state[to].push(item);
+    }
+  });
+
+
+  const lastItems = Object.keys(state).map((key) => state[key].pop());
+  return lastItems.join("")
+};
+
+console.log(solution());
